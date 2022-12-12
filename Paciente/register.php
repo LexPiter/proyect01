2cd98c1864858f1a4502fd4e7cf44b36eec85b47
LexPiter <51531544+LexPiter@users.noreply.github.com>
2022-12-12 01:13:36 -0500

    Update register.php

--- Paciente/register.php
+++ Paciente/register.php
@@ -117,8 +117,7 @@
 
 
 <script>
-//ejemplo datepiker en bootstrap tomado de:
-//http://formvalidation.io/examples/bootstrap-datepicker/
+
 
 $(document).ready(function() {
     $('#datePicker')
@@ -129,13 +128,6 @@ $(document).ready(function() {
         });
 });
 
-/*Basic example
-Embedding a date picker
-Setting date in a range
-Auto closing the date picker
-Tweets by @formvalidation
-Download Report bug
 
-© 2013 - 2016 Nguyen Huu Phuoc. All rights reserved.*/
 </script>
 
